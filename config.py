99b7d66b026e4a17b46755d5eaeb06654b7ed7a5
Nikhil <nikhil.thengane@valuematrix.ai>
2026-05-11 23:43:51 +0530

    Initial Selenium automation project

---
+++ config.py
@@ -0,0 +1,5 @@
+BASE_URL = "https://adnabu-store-assignment1.myshopify.com"
+
+STORE_PASSWORD = "AdNabuQA"
+
+PRODUCT_URL = "https://adnabu-store-assignment1.myshopify.com/products/the-3p-fulfilled-snowboard"
---
+++ utils/driver_factory.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def get_driver():
+
+    options = webdriver.ChromeOptions()
+
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-popup-blocking")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
+    driver.implicitly_wait(0)
+
+    return driver
